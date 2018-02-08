f61ae16ff91ab9e6e2ffeb0f7c4382e419f4dab3
Smiti <smitikaul@gmail.com>
2018-02-08 16:47:14 -0500

    need to map primary_cancer_type to discrete vars

---
+++ code/to_discrete.py
@@ -0,0 +1,4 @@
+import pandas as pd
+df = pd.read_csv('C:/Users/kauls15/Desktop/github/genie/data/derived/data_clinical_sample.txt')
+# df = pd.read_csv('C:\Users\kauls15\Desktop\github\genie\data\derived\data_clinical_sample.txt')
+# data = open("C:\Users\kauls15\Desktop\github\genie\data\derived\data_clinical_sample.txt", "r+")
